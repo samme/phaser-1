d83932ffc3d731c8ae809b38515e8d9cee2fbd27
samme <samme@users.noreply.github.com>
2020-03-26 08:08:02 -0700

    Separate script

---
+++ main.js
@@ -0,0 +1,40 @@
+var GAME_WIDTH = 1920;
+var GAME_HEIGHT = 1080;
+var loadingText;
+var GameScene = new Phaser.Class({
+  Extends: Phaser.Scene,
+  preload: function() {
+    this.load.image('sky', '13219.jpg');
+    // const width = this.cameras.main.width;
+    // const height = this.cameras.main.height;
+
+    // this.load.image('sky', '13219.jpg');
+    // loadingText = this.make.text({
+    //     x: width / 2,
+    //     y: height / 2 - 50,
+    //     text: 'Loading...',
+    //     style: {
+    //         font: '20px monospace',
+    //         fill: '#ffffff'
+    //     }
+    // });
+
+    // loadingText.setOrigin(0.5, 0.5);
+  },
+  create: function() {
+    this.logo = this.add.image(0, 0, 'sky').setOrigin(0);
+  }
+});
+
+var config = {
+  type: Phaser.AUTO,
+  scale: {
+    width: GAME_WIDTH,
+    height: GAME_HEIGHT
+  },
+
+  parent: 'phaser-example',
+  scene: GameScene
+};
+
+var game = new Phaser.Game(config);
