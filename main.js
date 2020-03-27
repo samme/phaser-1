617532b29d1a4b485dc01b1b470b7de3fbdddd99
samme <samme@users.noreply.github.com>
2020-03-27 12:22:57 -0700

    Loading text etc.

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var GAME_WIDTH = 1920;
 var GAME_HEIGHT = 1080;
+var GAME_RECT = Object.assign(
+  new Phaser.Geom.Rectangle(0, 0, GAME_WIDTH, GAME_HEIGHT),
+  { originX: 0, originY: 0 }
+);
 var loadingText;
+var Center = Phaser.Display.Align.In.Center;
 
 var debug = document.getElementById('debug');
 
@@ -12,20 +17,23 @@ var GameScene = new Phaser.Class({
     this.load.image('sky', '13219.jpg');
 
     loadingText = this.make.text({
-      x: this.cameras.main.centerX,
-      y: this.cameras.main.centerY - 50,
       text: 'Loading...',
       style: {
-        font: '20px monospace',
+        font: '120px monospace',
         fill: '#ffffff'
       }
     });
 
     loadingText.setOrigin(0.5, 0.5);
+
+    Center(loadingText, GAME_RECT, 0, -50);
   },
 
   create: function() {
     // this.logo = this.add.image(0, 0, 'sky').setOrigin(0);
+
+    loadingText.setText('Done');
+
     this.add.image(0, 0, '1920x1080').setOrigin(0, 0);
 
     this.cameras.main.setOrigin(0, 0);
@@ -35,12 +43,13 @@ var GameScene = new Phaser.Class({
 
   update: function() {
     debug.innerText = [
-      'canvasBounds: ' + this.scale.canvasBounds,
+      'canvasBounds: ' + JSON.stringify(this.scale.canvasBounds),
       'gameSize:     ' + this.scale.gameSize,
       'parentSize:   ' + this.scale.parentSize,
       'displaySize:  ' + this.scale.displaySize,
-      'displayScale: ' + this.scale.displayScale,
-      'camera:       ' + [this.cameras.main.scrollX, this.cameras.main.scrollY]
+      'displayScale: ' + JSON.stringify(this.scale.displayScale),
+      'camera:       ' + JSON.stringify(this.cameras.main.toJSON()),
+      'worldView:    ' + JSON.stringify(this.cameras.main.worldView)
     ].join('\n');
   }
 });
@@ -49,8 +58,8 @@ function checkOriention(orientation) {
   console.log('checkOrientation', orientation);
 
   var displaySize = this.scale.displaySize;
-  const displayScale = this.scale.displayScale;
-  const cam = this.cameras.main;
+  var displayScale = this.scale.displayScale;
+  var cam = this.cameras.main;
 
   // Force update displaySize.aspectRatio
   // https://github.com/photonstorm/phaser/issues/4971
@@ -67,7 +76,7 @@ function checkOriention(orientation) {
 
   console.assert(
     Phaser.Math.Fuzzy.Equal(displayScale.x, displayScale.y),
-    displayScale
+    'Scaled proportionally (x ≈ y)'
   );
 }
 
