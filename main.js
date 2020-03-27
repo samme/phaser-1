9262dfd0057d4c27fb5dc877a540ab13c588002b
samme <samme@users.noreply.github.com>
2020-03-27 07:46:11 -0700

    Rotation etc.

--- main.js
+++ main.js
@@ -6,39 +6,70 @@ var debug = document.getElementById('debug');
 
 var GameScene = new Phaser.Class({
   Extends: Phaser.Scene,
+
   preload: function() {
     this.load.image('1920x1080');
     this.load.image('sky', '13219.jpg');
-    // const width = this.cameras.main.width;
-    // const height = this.cameras.main.height;
 
-    // this.load.image('sky', '13219.jpg');
-    // loadingText = this.make.text({
-    //     x: width / 2,
-    //     y: height / 2 - 50,
-    //     text: 'Loading...',
-    //     style: {
-    //         font: '20px monospace',
-    //         fill: '#ffffff'
-    //     }
-    // });
+    loadingText = this.make.text({
+      x: this.cameras.main.centerX,
+      y: this.cameras.main.centerY - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff'
+      }
+    });
 
-    // loadingText.setOrigin(0.5, 0.5);
+    loadingText.setOrigin(0.5, 0.5);
   },
 
   create: function() {
     // this.logo = this.add.image(0, 0, 'sky').setOrigin(0);
     this.add.image(0, 0, '1920x1080').setOrigin(0, 0);
+
+    this.cameras.main.setOrigin(0, 0);
+
+    this.scale.on('orientationchange', checkOriention, this);
   },
 
   update: function() {
     debug.innerText = [
-      'gameSize:    ' + this.scale.gameSize,
-      'parentSize:  ' + this.scale.parentSize,
-      'displaySize: ' + this.scale.displaySize
+      'canvasBounds: ' + this.scale.canvasBounds,
+      'gameSize:     ' + this.scale.gameSize,
+      'parentSize:   ' + this.scale.parentSize,
+      'displaySize:  ' + this.scale.displaySize,
+      'displayScale: ' + this.scale.displayScale,
+      'camera:       ' + [this.cameras.main.scrollX, this.cameras.main.scrollY]
     ].join('\n');
   }
 });
+
+function checkOriention(orientation) {
+  console.log('checkOrientation', orientation);
+
+  var displaySize = this.scale.displaySize;
+  const displayScale = this.scale.displayScale;
+  const cam = this.cameras.main;
+
+  // Force update displaySize.aspectRatio
+  // https://github.com/photonstorm/phaser/issues/4971
+
+  if (orientation === Phaser.Scale.PORTRAIT) {
+    displaySize.aspectRatio = GAME_HEIGHT / GAME_WIDTH;
+    this.scale.setGameSize(GAME_HEIGHT, GAME_WIDTH);
+    cam.setAngle(90).setScroll(0, GAME_HEIGHT);
+  } else if (orientation === Phaser.Scale.LANDSCAPE) {
+    displaySize.aspectRatio = GAME_WIDTH / GAME_HEIGHT;
+    this.scale.setGameSize(GAME_WIDTH, GAME_HEIGHT);
+    cam.setAngle(0).setScroll(0, 0);
+  }
+
+  console.assert(
+    Phaser.Math.Fuzzy.Equal(displayScale.x, displayScale.y),
+    displayScale
+  );
+}
 
 var config = {
   type: Phaser.AUTO,
